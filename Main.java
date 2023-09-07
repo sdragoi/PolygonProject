cf28d96cb9426c8daac5ef90eb3ca6218a5c979c
Chloe <80842883+chloep23@users.noreply.github.com>
2023-09-07 17:09:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,285 +1,181 @@
-//Main Runtime Class
-//5/23/2022
+// Main Class
+// Date: May 23, 2022
 
-package application;
+package com.example.polyproj;
 import javafx.animation.AnimationTimer;
-import javafx.animation.Timeline;
-import javafx.animation.KeyFrame;
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Duration;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
-import javafx.scene.layout.*;
 
 public class Main extends Application {
-	
-	
-	public static void main(String[] args) {
-		launch();
- 
-	}
-	
-	AnimationTimer timer;
-	Pane root = new Pane();
-	List<ShapeClass> drop = new ArrayList();
-	double mouseX;
-	Player cont;
-	double speed;
-	double falling;
-	Label lblMissed;
-	Label lblHit;
-	Label lblIAM;
-	Label gameOver;
-	Label startMessage;
-	Label title;
-	int hit;
-	int missed;
-	int iam;
-	double intAng = 60;
- 
-	
- 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
- 
-		lblHit = new Label("Polygons Eaten: 0");
-		lblHit.setLayoutX(10);
-		lblHit.setLayoutY(30);
-		 hit = 0;
-		
-		lblMissed = new Label("Missed: 0");
-		lblMissed.setLayoutX(10);
-		lblMissed.setLayoutY(10);
-		missed = 0;
-		
-		lblIAM = new Label("Interior Angle Measure: 60");
-		lblIAM.setLayoutX(10);
-		lblIAM.setLayoutY(50);
-		iam = 60;
-		
-		gameOver = new Label("");
-		gameOver.setLayoutX(90);
-		gameOver.setLayoutY(230);
-		gameOver.setFont(Font.font("Chalkduster", FontWeight.BOLD, FontPosture.REGULAR, 75));
-		
-		startMessage = new Label("");
-		startMessage.setLayoutX(50);
-		startMessage.setLayoutY(230);
-		startMessage.setFont(Font.font("Chalkduster", FontWeight.BOLD, FontPosture.REGULAR, 15));
-		
-		
-		
-		
- 
-		speed = 0.25;
-		falling = 500;
-		
-		Canvas canvas = new Canvas(545,600);
-        GraphicsContext context = canvas.getGraphicsContext2D();
- 
-		Timeline timeline = new Timeline(new KeyFrame(Duration.millis(falling), event -> {
- 
-			//speed += falling / 3000;
-			drop.add(shape());
-			root.getChildren().add(((Node)drop.get(drop.size() -1)));
-        }));
-		
-		root.getChildren().addAll(startMessage);
-		startMessage.setText("Welcome to the Polygon Game! In this game, you will need to \n"
-				+ "eat polygons in order to increase your interior angle measure (IAM) and \n"
-				+ "eventually become a circle! Avoid the even-sided polygons, as they \n"
-				+ "will decrease your IAM, but try to eat the prime-sided ones-- \n"
-				+ "they will help you become a circle! Good luck! "); 
-		
- 
-		timeline.setCycleCount(1000);
-		timeline.play();
- 
-		
- 
-		timer = new AnimationTimer() {
- 
-			@Override
-			public void handle(long arg0) {
-				cont.setLayoutX(mouseX);
-				cont.setLayoutY(480);
-				
-				Properties prop = new Properties();
-		    	try {
-		    		FileInputStream input = new FileInputStream("resources/config.properties");
-		    		prop.load(input);
-		    	} catch (FileNotFoundException e) {
-		    		// TODO Auto-generated catch block
-		    		e.printStackTrace();
-		    	} catch (IOException e) {
-		    		// TODO Auto-generated catch block
-		    		e.printStackTrace();
-		    	}
-				 
-				for(int i = 0; i < drop.size(); i++) {
-					//drop.get(i).render(context);
-					((ShapeClass) drop.get(i)).setLayoutY(((ShapeClass) drop.get(i)).getLayoutY() + speed + ((ShapeClass) drop.get(i)).getLayoutY() / 150 );
-					//if get dropped into square
-					if((((ShapeClass) drop.get(i)).getLayoutX() > cont.getLayoutX() && ((ShapeClass) drop.get(i)).getLayoutX() < cont.getLayoutX() + 70) &&
-							((ShapeClass) drop.get(i)).getLayoutY() >= 480	) {					
-						if (drop.get(i).interiorAngle == 60) {
-		                    cont.interiorAngle += drop.get(i).interiorAngle / 3;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 90) {
-		                    cont.interiorAngle -= drop.get(i).interiorAngle / 5;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 108) {
-		                    cont.interiorAngle += drop.get(i).interiorAngle / 3;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 120) {
-		                    cont.interiorAngle -= drop.get(i).interiorAngle / 5;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 128.57) {
-		                    cont.interiorAngle += drop.get(i).interiorAngle / 3;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 135) {
-		                    cont.interiorAngle -= drop.get(i).interiorAngle / 5;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 140) {
-		                    cont.interiorAngle += 0;
-		                    intAng = cont.interiorAngle;
-		                }
-		                if (drop.get(i).interiorAngle == 144) {
-		                    cont.interiorAngle -= drop.get(i).interiorAngle / 5;
-		                    intAng = cont.interiorAngle;
-		                }
-		                //System.out.println("Interior angle= " + intAng + " . It was changed by" + drop.get(i).interiorAngle);
-		                intAng = cont.changeAngle(intAng);
-		                intAng = cont.getnewAngle(intAng);
-		                cont.setImage(cont.changeImage(intAng));
-		                hit += 1;
-		                root.getChildren().remove(startMessage);
-						lblHit.setText("Polygons Eaten: " + String.valueOf(hit));
-						lblIAM.setText("Interior Angle Measure: " + intAng);
-						root.getChildren().remove(((ShapeClass) drop.get(i)));
-						drop.remove(i);
-						
-						if(intAng == 0){
-							timer.stop();
-							timeline.stop();
-							root.getChildren().removeAll(drop);
-							root.getChildren().addAll(gameOver);
-							gameOver.setText("GAME OVER");
-							
-						}
-						else if(intAng > 180) {
-							timer.stop();
-							timeline.stop();
-							root.getChildren().removeAll(drop);
-							root.getChildren().addAll(gameOver);
-							gameOver.setText("CIRCLE STATUS REACHED!");
-							
-						}
-					}
-		 
-					//if missed remove
-					else if(((ShapeClass) drop.get(i)).getLayoutY() >= 590) {
-						root.getChildren().remove(startMessage);
-						root.getChildren().remove(((ShapeClass) drop.get(i)));
-						drop.remove(i);
-						missed += 1;
-						lblMissed.setText("Missed: " + String.valueOf(missed));
-						
-						
-					}
-				}
- 
-			}
- 
-			
-		};
-		timer.start();	
- 
-		cont = new Player(60);
+  public static void main(String[] args) {
+      launch();
+  }
+  // Method moves the shapes down the screen and appropriately tests collision
+  public void dropShapes(Player player, ArrayList<Shape> shapeRow) {
 
-		
-		//cont.render(context);
-		
-		
-		
- 
-		root.getChildren().addAll(cont, lblMissed, lblHit, lblIAM);
- 
-		Scene scene = new Scene(root, 700, 600);
- 
-		scene.setOnMouseMoved(e -> {
-			mouseX = e.getX();
-		});
-		
- 
-		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-		
- 
-	}
- 
-	public ShapeClass shape() {
-		ShapeClass shape = new ShapeClass(1);
-		shape.setLayoutX(rand(0, 400));
-		shape.setLayoutY(1);
-		shape.autosize();
-		//shape.render(context);
-		//shape.shapeType
-		return shape;
-	}
- 
-	public Rectangle rectangle() {
-		Rectangle rectangle = new Rectangle();
-		rectangle.setLayoutX(200);
-		rectangle.setLayoutY(550);
-		rectangle.setHeight(50);
-		rectangle.setWidth(70);
-		rectangle.setFill(Color.GREEN);
-		return rectangle;
- 
-	}
+      for (int i = 0; i < shapeRow.size(); i++) {
+          Shape shape = shapeRow.get(i);
+	      
+          if (player.overlaps(shape)) {
+              if (shape.interiorAngle == 60) {
+                  player.interiorAngle += shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 90) {
+                  player.interiorAngle -= shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 108) {
+                  player.interiorAngle += shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 120) {
+                  player.interiorAngle -= shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 128.57) {
+                  player.interiorAngle += shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 135) {
+                  player.interiorAngle -= shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 140) {
+                  player.interiorAngle += 0;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              if (shape.interiorAngle == 144) {
+                  player.interiorAngle -= shape.interiorAngle / 3;
+                  System.out.println(player.interiorAngle);
+              }
+		  
+              shapeRow.clear();
+          }
+      }
+	  
+      // Sets speed for falling objects
+      for (int i = 0; i < shapeRow.size(); i++) {
+          shapeRow.get(i).position.add(0,2);
+      }
+  }
 	
-	
- 
-	public int rand(int min, int max) {
-		return (int)(Math.random() * max + min);
-	}
-	
-	public void gameUpdate(GraphicsContext context){
- 
-		
-	}
- 
+  // Method creates an ArrayList with five shapes in it
+  public ArrayList<Shape> shapeCreation () {
+      ArrayList<Shape> shapeRow = new ArrayList<Shape>();
+      int shapeCounter = 5;
+      for (int i = 0; i < shapeCounter; i++) {
+          Shape shape = new Shape((int)Math.ceil(Math.random() * 8), 60*(i) + (i * 50));
+          shape.position.set(50);
+          shapeRow.add(shape);
+      }
+      return shapeRow;
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+      // Sets up window for GUI
+      BorderPane root = new BorderPane();
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.setResizable(false);
+
+      //Initializes game icon and title
+      Image icon = new Image("file:\\C:\\Users\\julma\\IdeaProjects\\demo\\PolyProj\\src\\circle.png");
+      stage.getIcons().add(icon);
+      stage.setTitle("Polygon Game");
+
+      // Creates a canvas for the game to run on
+      Canvas canvas = new Canvas(545,600);
+      GraphicsContext context = canvas.getGraphicsContext2D();
+
+      // Fills the page with the color white
+      root.setCenter(canvas);
+      context.setFill(Color.WHITE);
+      context.fillRect(0,0,600,600);
+
+      // Creates the player
+      Player player = new Player();
+      player.position.set(250,500);
+      player.setImage("C:\\Users\\julma\\IdeaProjects\\demo\\PolyProj\\src\\tri.jpg");
+      player.render(context);
+
+      // Creates an ArrayList to store and read the inputs of the player when the game starts
+      ArrayList<String> inputList = new ArrayList<String>();
+      scene.setOnKeyPressed(
+              (KeyEvent event) -> {
+                  String keyName = event.getCode().toString();
+                  if (!inputList.contains(keyName)) {
+                      inputList.add(keyName);
+                  }
+              }
+      );
+      scene.setOnKeyReleased(
+              (KeyEvent event) -> {
+                  String keyName = event.getCode().toString();
+                  inputList.remove(keyName);
+              }
+      );
+
+      // Creates a random ArrayList of shapes falling from the sky
+      ArrayList<Shape> shapeRow = shapeCreation();
+
+      // Controls the player's speed and the falling of the shapes
+      AnimationTimer gameLoop = new AnimationTimer() {
+          public void handle(long nanoTime) {
+              // Declares player speed
+              double speedOfPlayer = 500;
+		  
+              // What happens if the player moves left:
+              if (inputList.contains("LEFT")){
+                  player.velocity.add(-speedOfPlayer,0);
+              }
+		  
+              // What happens if the player moves right:
+              if (inputList.contains("RIGHT")){
+                  player.velocity.add(speedOfPlayer,0);
+              }
+              // Sets the speed for the player per input press
+              player.velocity.scalarMult(1/60.0);
+              player.position.add(player.velocity);
+
+
+              // Clears canvas
+              context.setFill(Color.WHITE);
+              context.fillRect(0, 0, 600, 600);
+
+              // Initiates falling shapes
+              dropShapes(player, shapeRow);
+              for (Shape shape : shapeRow) {
+                      shape.render(context);
+              }
+		  
+              // Rendering in the player
+              player.render(context);
+
+          }
+      };
+	  
+      // Beginning gameLoop
+      gameLoop.start();
+
+      // Starts showing the GUI
+      stage.show();
+  }
 }
